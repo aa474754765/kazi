c2dca2f59f297d9ffa56a26073800c8e4b7ec9ec
Jitao Wang <474754765@qq.com>
2022-12-16 14:43:03 +0800

    install element-ui

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Element, { size: 'medium', zIndex: 3000 })
 
 /* eslint-disable no-new */
 new Vue({
