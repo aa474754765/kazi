edc41ec87d46e0f67f0b6e04d51e741f83d97f4b
Jitao Wang <474754765@qq.com>
2023-01-03 10:11:57 +0800

    disable space-before-function-paren

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     "vue/comment-directive": "off",
+    "space-before-function-paren": 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
